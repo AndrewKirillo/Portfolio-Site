b35b0abc6a1883de623097f57573bfdd09c93c88
andrewkirillo <kirillov.edu@gmail.com>
2019-12-28 18:47:58 -0500

    working on sidebar

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import "./SideNav.css";
 
 export default class SideNav extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: 0
+            active: "link-0"
         }
         
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick = (sectionNum) => {
-        return () => {
+        return (e) => {
+            document.getElementById(this.state.active).classList.remove("active");
+            const targetId = e.target.id;
+            document.getElementById(targetId).classList.add("active");
+            this.setState({active: targetId});
             const section = document.getElementById("section-" + sectionNum);
             window.scrollBy(0, section.getBoundingClientRect().top);
-            this.setState({ active: sectionNum });
+            
         }
     }    
 
     render() {
         return (
-            <div>
-                <a id="link-0" className="active" onClick={this.handleClick("0")}>0</a>
-                <a id="link-1" onClick={this.handleClick("1")}>1</a>
-                <a id="link-2" onClick={this.handleClick("2")}>2</a>
-                <a id="link-3" onClick={this.handleClick("3")}>3</a>
+            <div className="sidebar">
+                <div id="link-0" className="active" onClick={this.handleClick("0")}/>
+                <div id="link-1" onClick={this.handleClick("1")}/>
+                <div id="link-2" onClick={this.handleClick("2")}/>
+                <div id="link-3" onClick={this.handleClick("3")}/>
             </div>
         )
     }
